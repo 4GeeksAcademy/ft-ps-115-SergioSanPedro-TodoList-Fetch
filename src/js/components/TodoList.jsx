c2a15507055f2a2b2af31587d08925c06faa4dd3
Sergio San Pedro Álvarez <sergiospalvarez@hotmail.com>
2025-07-16 21:08:37 +0000

    todolist con fetch

---
+++ src/js/components/TodoList.jsx
@@ -0,0 +1,105 @@
+import { useEffect, useState } from "react";
+
+const URLApi = 'https://playground.4geeks.com/todo/users/sergio90';
+
+
+export const TodoList = () => {
+
+	const [input, setInput] = useState('')
+	const [taskList, setTaskList] = useState([]);
+
+	const handleKeyUp = (e) => {
+		if (e.key === 'Enter') {
+			createTask();
+		}
+	};
+
+	const deleteTarea = async (id) => {
+
+		const response = await fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
+			method: 'DELETE'
+		});
+
+		setTaskList(taskList.filter(task => task.id !== id));
+
+	};
+
+
+	const createTask = async () => {
+		const response = await fetch('https://playground.4geeks.com/todo/todos/sergio90', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				label: input,
+				is_done: false
+			})
+		})
+		const data = await response.json();
+		getTask();
+		setInput('');
+
+	}
+
+
+	const getTask = async () => {
+		const response = await fetch(URLApi);
+
+		if (!response.ok) {
+			console.log('Crea usurario atontao')
+			createUser()
+		}
+		const data = await response.json()
+		setTaskList(data.todos);
+
+	}
+
+	const createUser = async () => {
+		await fetch(URLApi, { method: 'POST' })
+	}
+
+	useEffect(() => {
+		getTask()
+	}, [])
+
+
+
+	return (
+		<>
+
+			<h1 className="text-center mt-4 titulo">LISTA DE TAREAS</h1>
+
+			<div className="container my-4 border-2 d-flex justify-content-center ">
+
+				<div className="row p-3 mx-5 w-50 bg-dark rounded-2">
+					<input
+						className="input rounded-2 fw-bold"
+						type="text"
+						placeholder="Introduce la tarea"
+						onChange={(e) => setInput(e.target.value)}
+						onKeyUp={handleKeyUp}
+						value={input}
+					/>
+
+					<span className="text-white text-center my-2">
+						Te {taskList.length === 1 ? 'queda' : 'quedan'} {taskList.length} {taskList.length === 1 ? 'tarea' : 'tareas'} aún por completar. {taskList.length > 1 ? 'Espabila' : ''}
+
+					</span>
+
+					{
+						taskList.map((task) => (
+							<div className="d-flex justify-content-between bg-white align-items-center mt-1 border border-2 tarea rounded-2 fw-bold">
+								<span key={task.id}>{task.label}</span>
+								<button onClick={() => deleteTarea(task.id)} className="btn">❌</button>
+							</div>
+
+						))
+					}
+
+
+				</div>
+			</div>
+		</>
+	)
+};
